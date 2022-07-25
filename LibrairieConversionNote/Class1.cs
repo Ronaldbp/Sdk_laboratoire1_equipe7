08376fe1e6764b3cb04c371e5dc9a98e6f3be012
Ronaldbp <ronaldbp@outlook.com>
2022-07-25 12:56:29 -0400

    Nouvellelibrairie

---
+++ LibrairieConversionNote/Class1.cs
@@ -0,0 +1,55 @@
+﻿namespace LibrairieConversionNote
+{
+    public class ConversionAlphabetiquetoNote
+    {
+        public string transform()
+        {
+            string note = "";
+
+            switch(note)
+
+            {
+                case "A": 
+                    return "LA";
+                        break;
+
+                case "B":
+                    return "SI";
+                    break;
+
+                case "C":
+                    return "DO";
+                    break;
+
+                case "D":
+                    return "RE";
+                    break;
+
+                case "E":
+                    return "MI";
+                    break;
+
+
+                case "G":
+                    return "FA";
+                    break;
+
+
+                case "H":
+                    return "SOL";
+                    break;
+
+
+
+
+
+                default: 
+                    return "error";
+                    break;
+            }
+            
+        
+        }
+
+    }
+}
